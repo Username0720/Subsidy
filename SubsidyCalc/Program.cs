219464075bcd70f8b940ce68e5b2657215137cad
User <39237166+Username0720@users.noreply.github.com>
2021-04-03 18:46:20 +0300

    first commit

---
+++ SubsidyCalc/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SubsidyCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tariff tariff = new Tariff()
+            {
+                ServiceId = 1293,
+                HouseId = 11,
+                PeriodBegin = DateTime.Now.AddMonths(-2),
+                PeriodEnd = DateTime.Now.AddMonths(-1),
+                Value = 10
+            };
+            Volume volume = new Volume()
+            {
+                ServiceId = 1293,
+                HouseId = 11,
+                Month = DateTime.Now.AddMonths(-1),
+                Value = 10
+            };
+            Charge charge = new Charge();
+            SubsidyCalculation subsidyCalculation = new SubsidyCalculation();
+            try
+            {
+                subsidyCalculation.OnNotify += new EventHandler<string>(NewNotify);
+                charge = subsidyCalculation.CalculateSubsidy(volume, tariff);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.WriteLine(charge.HouseId);
+            Console.WriteLine(charge.Value);
+            subsidyCalculation.OnNotify += new EventHandler<string>(NewNotify);
+        }
+        static void NewNotify(object sender, string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ SubsidyCalc/SubsidyCalculation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubsidyCalc
+{
+    class SubsidyCalculation : Charge, ISubsidyCalculation
+    {
+        public event EventHandler<string> OnNotify;
+        public event EventHandler<Tuple<string, Exception>> OnException;
+        public Charge CalculateSubsidy(Volume volumes, Tariff tariff)
+        {
+            Charge charges = new Charge();
+            try
+            {
+                if (volumes.HouseId != tariff.HouseId)
+                {
+                    OnException?.Invoke(sender: "id-шники не совпадают",
+                        Tuple.Create("volumes.HouseId != tariff.HouseId", new Exception()));
+                    throw new Exception("Неправильные данные");
+                }
+                decimal v = volumes.Value;
+                decimal t = tariff.Value;
+                OnNotify?.Invoke(sender: $"Расчёт начат в {DateTime.Now}", "");
+                charges.Value = v * t;
+                charges.ServiceId = volumes.ServiceId;
+                charges.HouseId = volumes.HouseId;
+                charges.Month = volumes.Month;
+            }
+            catch (Exception e)
+            {
+                OnException?.Invoke(sender: "Какая-то ошибка", Tuple.Create("Неизвестная ошибка", e));
+                throw;
+            }
+            OnNotify?.Invoke(sender: $"Расчёт успешно завершён в {DateTime.Now}", $"");
+            return charges;
+        }
+    }
+}
